81f6c32ca950ebed6c72b32ac3a75f953504ad2e
cevheri <cevheribozoglan@gmail.com>
2023-09-11 01:15:49 +0300

    add firebase web support

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_favorite_app/presentation/state/app_state.dart';
 import 'package:flutter_favorite_app/routes/page_route.dart';
@@ -7,7 +8,7 @@ import 'firebase_options.dart';
 
 
 void main() async {
-
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -16,8 +17,11 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+  FirebaseAnalyticsObserver(analytics: analytics);
 
   @override
   Widget build(BuildContext context) {
