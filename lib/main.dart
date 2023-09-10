1f67a310038f2a37cc6157575b144f49923e76d0
cevheri <cevheribozoglan@gmail.com>
2023-09-11 00:44:23 +0300

    add firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_favorite_app/presentation/state/app_state.dart';
 import 'package:flutter_favorite_app/routes/page_route.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
